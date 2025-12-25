7bb1cb3b8272132b8da3b47f4cdae5ce0b7dd1c0
Linkin Park Fan <koarnick@gmail.com>
2025-12-25 09:32:51 +0300

    Added Config.cs

---
+++ Pikulator/Config.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Pikulator
+{
+    public class Config
+    {
+        public static string path = Path.Combine(Program.path + "/Config.json");
+        public static Config Read()
+        {
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+        }
+        public void Write()
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+    }
+}
