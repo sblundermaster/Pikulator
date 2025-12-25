d19bf2d976bac0b91bfebe6bb5a132265fdcba94
Linkin Park Fan <koarnick@gmail.com>
2025-12-25 09:43:09 +0300

    Created List<string, ICommands>

--- Pikulator/Program.cs
+++ Pikulator/Program.cs
@@ -3,6 +3,10 @@
     public class Program
     {
         public static string path = "";
+        public static Dictionary<string, ICommand> commands = new Dictionary<string, ICommand>()
+        {
+
+        };
         public static void Main(string[] args)
         {
 
